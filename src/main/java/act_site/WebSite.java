30588d478508cbf5f081a6562501260a2b3240e7
Gelin Luo <greenlaw110@gmail.com>
2017-01-24 23:46:06 +1100

    i18n

--- src/main/java/act_site/WebSite.java
+++ src/main/java/act_site/WebSite.java
@@ -1,12 +1,34 @@
 package act_site;
 
+import act.app.conf.AppConfigurator;
 import act.boot.app.RunApp;
+import act.util.ActContext;
+import act.view.TemplatePathResolver;
 import org.osgl.mvc.annotation.GetAction;
 import org.osgl.mvc.result.Result;
+
+import java.util.Locale;
 
 import static act.controller.Controller.Util.render;
 
 public class WebSite {
+
+    public static class MyConfiguration extends AppConfigurator {
+        @Override
+        public void configure() {
+            templatePathResolver(new TemplatePathResolver() {
+                @Override
+                protected String resolveTemplatePath(String path, ActContext context) {
+                    String resolved = super.resolveTemplatePath(path, context);
+                    Locale locale = context.locale(true);
+                    if ("zh".equals(locale.getLanguage())) {
+                        resolved = "/cn" + (resolved.startsWith("/") ? "" : "/") + resolved;
+                    }
+                    return resolved;
+                }
+            });
+        }
+    }
 
     @GetAction
     public void home() {
