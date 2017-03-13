50f666fb0838eb22c49e61985663d064070539d8
Gelin Luo <greenlaw110@gmail.com>
2017-03-13 12:48:55 +1100

    add release notes link; update to latest act version

--- src/main/java/act_site/DocLoader.java
+++ src/main/java/act_site/DocLoader.java
@@ -2,24 +2,14 @@ package act_site;
 
 import act.app.ActionContext;
 import act.app.App;
-import act.controller.Controller;
 import act.controller.ParamNames;
-import act.handler.RequestHandler;
-import act.handler.RequestHandlerResolverBase;
-import act.handler.builtin.StaticResourceGetter;
 import act.handler.builtin.controller.FastRequestHandler;
 import act.view.RenderTemplate;
 import org.osgl.http.H;
-import org.osgl.mvc.result.NotFound;
-import org.osgl.mvc.result.Redirect;
-import org.osgl.mvc.result.RenderBinary;
-import org.osgl.mvc.result.Result;
 import org.osgl.util.S;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Locale;
 
 /**
@@ -42,24 +32,29 @@ public class DocLoader extends FastRequestHandler {
 
     private RenderTemplate _handle(String path, ActionContext context) {
         Locale locale = context.locale(true);
-        String sLocale = locale.getLanguage();
-        String lang = ("zh".equals(sLocale)) ? "cn" : "en";
-        StringBuilder sb = S.builder("https://raw.githubusercontent.com/actframework/act-doc/master/").append(lang);
-        if (path.contains("#")) {
-            String[] pa = path.split("#");
-            String pa1 = pa[0];
-            sb.append(pa1);
-            if (!pa1.endsWith(".md")) {
-                sb.append(".md");
-            }
-            sb.append("#").append(pa[1]);
+        String newPath = null;
+        if (path.toLowerCase().startsWith("/release_notes")) {
+            newPath = "https://raw.githubusercontent.com/actframework/act-doc/master/RELEASE_NOTES.md";
         } else {
-            sb.append(path);
-            if (!path.endsWith(".md")) {
-                sb.append(".md");
+            String sLocale = locale.getLanguage();
+            String lang = ("zh".equals(sLocale)) ? "cn" : "en";
+            StringBuilder sb = S.builder("https://raw.githubusercontent.com/actframework/act-doc/master/").append(lang);
+            if (path.contains("#")) {
+                String[] pa = path.split("#");
+                String pa1 = pa[0];
+                sb.append(pa1);
+                if (!pa1.endsWith(".md")) {
+                    sb.append(".md");
+                }
+                sb.append("#").append(pa[1]);
+            } else {
+                sb.append(path);
+                if (!path.endsWith(".md")) {
+                    sb.append(".md");
+                }
             }
+            newPath = sb.toString();
         }
-        String newPath = sb.toString();
         context.renderArg("docPath", newPath);
         context.templatePath("/doc.html");
         return RenderTemplate.get();
